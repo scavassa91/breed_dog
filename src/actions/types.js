df4edaec1505b012d8935670e2135647f2f7cddd
Danilo Scavassa <dan.scavassa@gmail.com>
2018-02-04 18:19:47 -0200

    Action types comments

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,18 @@
+/**
+ * @constant
+ * @type {string}
+ * @default get_all_breeds
+ */
 export const GET_ALL_BREEDS = 'get_all_breeds';
+/**
+ * @constant
+ * @type {string}
+ * @default get_rand_img
+ */
 export const GET_RAND_IMG = 'get_rand_img';
+/**
+ * @constant
+ * @type {string}
+ * @default breed_selected
+ */
 export const BREED_SELECTED = 'breed_selected';
